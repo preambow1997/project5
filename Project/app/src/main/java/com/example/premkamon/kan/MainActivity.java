f98412e0c87d5f305adafe9d270657b437d96b88
Bow <pream_bow1997@hotmail.com>
2016-09-30 12:16:20 +0700

    ..

--- Project/app/src/main/java/com/example/premkamon/kan/MainActivity.java
+++ Project/app/src/main/java/com/example/premkamon/kan/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txtName = (EditText)findViewById(R.id.editText2);
+        final EditText txtName1 = (EditText)findViewById(R.id.editText);
+
+        final Button subMitfrm = (Button)findViewById(R.id.btn);
+
+        final TextView ShowMsg = (TextView) findViewById(R.id.textView);
+        final TextView ShowMsg1 = (TextView) findViewById(R.id.textView3);
+
         Button btn = (Button)findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), Select.class);
                 startActivity(i);
+
+
             }
         });
     }
